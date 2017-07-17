c3585b30a0f113c1295bb8bc8db3783c4927659b
jinss <171432825@qq.com>
2017-07-17 10:38:09 +0800

    提交工程

---
+++ webFileSource/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  webFileSource
+//
+//  Created by jssName on 2017/7/17.
+//  Copyright © 2017年 jssName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
